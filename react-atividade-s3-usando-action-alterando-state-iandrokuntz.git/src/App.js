73689671d45a245a6a503fa4d42aaa4371fa6302
iandrokuntz <ianbertoluchi99@gmail.com>
2021-11-23 11:57:54 -0300

    atividade pronta

---
+++ react-atividade-s3-usando-action-alterando-state-iandrokuntz.git/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import InputUser from './components/InputUser';
+
+function App() {
+
+  return (
+
+    <div className="App">
+
+      <InputUser/>
+
+    </div>
+    
+  )
+}
+
+export default App;
---
+++ react-atividade-s3-usando-action-alterando-state-iandrokuntz.git/src/components/InputUser/index.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux"
+import { changeName } from "../../store/modules/actions";
+import "./style.css"
+
+const InputUser = () => {
+
+    const [newName, setNewName] = useState("")
+    const {name} = useSelector((state) => state.user)
+    const dispatch = useDispatch()
+
+    const handleClick = () => {
+        dispatch(changeName(newName))
+        setNewName("")
+      }
+
+    return(
+        <div>
+            <h1>Name: <h3>{name}</h3></h1>
+            <input type="text" value={newName} onChange={(evt) => setNewName(evt.target.value)}/>
+            <button onClick={handleClick}>Change</button>
+        </div>
+    )
+}
+
+export default InputUser
